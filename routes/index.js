8b34d0fad8b2ffe4b7ff322d84c006152afcf3db
peterjacobson <peterjacobson.nz@gmail.com>
2015-08-25 14:39:59 +1200

    implementing xero extended package

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var Xero = require('xero');
 
 var xero = new Xero(process.env.CONSUMER_KEY, process.env.CONSUMER_SECRET);
-
+console.log(xero);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
@@ -13,33 +13,40 @@ router.get('/', function(req, res, next) {
 
 /*POST form to Xero  */
 router.post('/invoice', function(req, res, next) {
-	var xeroRequest = {
-		Invoice: {
-			Type: "ACCREC",
-			Contact: {
-				ContactID: "",
-			},
-			DueDate: "",
-			LineItems: [
-				{
-					Description: "",
-					Quantity: "",
-					UnitAmount: "",
-					AccountCode: "",
-				}
-			],
-			Status: "AUTHORISED",
-		}
-	}
+	var invoiceData = {
+		Type: xero.Invoices.SALE,
+		Contact: {
+			Name: 'Luke Kavanagh'
+		},
+		Date: new Date(),
+		DueDate: '2015-08-23',
+		LineAmountTypes: xero.Invoices.EXCLUSIVE,
+		LineItems: [
+			{
+				Description: "Buns",
+				Quantity: 18,
+				UnitAmount: 120,
+				AccountCode: 200,
+			}
+		]
+	};
 	console.log('hihi');
-	xero.call('POST', 'https://api.xero.com/api.xro/2.0/Invoices', xeroRequest, function(err, json) {
-		if (err) {
-      log.error(err);
-      return res.json(400, {error: 'Unable to contact Xero'});
-    }
-    console.log(res);
-    return res.json(200, json);
-	}); 
+	xero.Invoices.create(invoiceData, function (err, invoice) {
+	  if (err) {
+	    res.json(err);
+	  }
+	  console.log(res);
+	  res.json(null, invoice);
+	});
+	// xero.call('POST', 'https://api.xero.com/api.xro/2.0/Invoices', xeroRequest, function(err, json) {
+	// 	if (err) {
+ //      log.error(err);
+ //      console.log(err);
+ //      return res.json(400, {error: 'Unable to contact Xero'});
+ //    }
+ //    console.log(res);
+ //    return res.json(200, json);
+	// }); 
 	res.redirect('/');
 });
 
