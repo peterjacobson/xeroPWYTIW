6407fa077681b663181edb964741af0602b1b097
peterjacobson <peterjacobson.nz@gmail.com>
2015-08-25 17:05:29 +1200

    got HTTP:302 from Xero Posting to /invoices in our new 'private' app, using SSL keys

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Xero = require('xero');
 
-var xero = new Xero(process.env.CONSUMER_KEY, process.env.CONSUMER_SECRET);
+var xero = new Xero(process.env.CONSUMER_KEY, process.env.CONSUMER_SECRET, process.env.RSA_PRIVATE_KEY);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,27 +14,25 @@ router.get('/', function(req, res, next) {
 /*POST form to Xero  */
 router.post('/invoice', function(req, res, next) {
 	var xeroRequest = {
-		Invoice: {
 			Type: "ACCREC",
 			Contact: {
-				ContactID: "",
+				Name: "Luke",
 			},
-			DueDate: "",
+			Date: "2015-08-25T00:00:00",
+			DueDate: "2015-08-25T00:00:00",
+			LineAmountTypes: "Exclusive",
 			LineItems: [
 				{
-					Description: "",
-					Quantity: "",
-					UnitAmount: "",
-					AccountCode: "",
+					Description: "Buns",
+					Quantity: 2,
+					UnitAmount: 3,
+					AccountCode: 200,
 				}
-			],
-			Status: "AUTHORISED",
-		}
+			]
 	}
-	console.log('hihi');
 	xero.call('POST', 'https://api.xero.com/api.xro/2.0/Invoices', xeroRequest, function(err, json) {
 		if (err) {
-      log.error(err);
+      // log.error(err);
       return res.json(400, {error: 'Unable to contact Xero'});
     }
     console.log(res);
