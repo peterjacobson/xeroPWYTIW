7396630e76b1a38e3905a2276e88d94e44ac5dcd
Joshua Vial <joshua@enspiral.com>
2015-08-27 22:33:31 +1200

    updating config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	xeroAdmin: {
-		email: 'peterjacobson.nz@gmail.com',
-		name: 'Pete'
+		email: 'joshua@enspiral.com',
+		name: 'Joshua'
 	},
-	invoiceProjectDescription: 'Orientation App Game',
-	projectIntroduction: "An app to gamify Enspiral's orientation process",
-	daysUntilPaymentDue: 30
+	invoiceProjectDescription: 'Individual monthly membership',
+	projectIntroduction: "Variable individual contributions in cobudget",
+	daysUntilPaymentDue: 3
 }
